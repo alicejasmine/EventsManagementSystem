c592960f8447cf243aa08a1acfb1abc258459823
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2023-03-16 13:57:52 +0100

    bll
    add crud method calls to bll

---
+++ src/BLL/LogicManager.java
@@ -0,0 +1,27 @@
+package BLL;
+
+import BE.Event;
+import DAL.EventDAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class LogicManager {
+    private EventDAO eventDAO=new EventDAO();
+
+    public Event createEvent(Event e) throws SQLException {
+        return eventDAO.createEvent(e);
+    }
+    public void deleteEvent(Event e){
+        eventDAO.deleteEvent(e.getId());
+    }
+
+    public void update(Event e){
+        eventDAO.updateEvent(e);
+    }
+
+    public List<Event> getAllEvents() throws SQLException {
+        return eventDAO.getAllEvents();
+    }
+
+}
