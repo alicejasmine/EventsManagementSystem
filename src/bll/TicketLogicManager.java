9bebce346f0853c258695e9d5a51a9302983d6dd
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2023-03-21 18:41:47 +0100

    Print QR code on ticket

--- src/bll/TicketLogicManager.java
+++ src/bll/TicketLogicManager.java
@@ -1,6 +1,7 @@
 package bll;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -9,10 +10,16 @@ import java.util.UUID;
 
 import be.Event;
 import be.Ticket;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
 import dal.*;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.text.*;
+import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -22,9 +29,10 @@ import org.apache.pdfbox.util.Matrix;
 public class TicketLogicManager {
 
     private TicketDAO ticketDAO = new TicketDAO();
+    private EventDAO eventDAO = new EventDAO();
 
-    public static void main(String[] args) throws IOException {
-        //writeEventInfoOnPDF();
+    public static void main(String[] args) throws IOException, WriterException {
+        //writeEventInfoOnPDF(eventDAO.getAllEvents().get(0),ticketDAO.getAllTickets().get(0));
         //for(int i = 0; i < 100; i++){
         //    System.out.println(generateType1UUID());}
     }
@@ -33,43 +41,43 @@ public class TicketLogicManager {
         return ticketDAO.getAllTickets();
     }
 
-    public void crateTicket(String customerName, String customerEmail, int eventID){
+    public void crateTicket(String customerName, String customerEmail, int eventID) {
         String str = generateType1UUID().toString();
         Ticket ticket = new Ticket(str, customerName, customerEmail, eventID);
         ticketDAO.createTicket(ticket);
     }
 
-    public static void writeEventInfoOnPDF(Event event, Ticket ticket) throws IOException {
+    /**
+     * Method to print event info on a ticket using Apache PDFbox libraries and QR code */
+    public static void writeEventInfoOnTicket(Event event, Ticket ticket) throws IOException, WriterException {
 
 
-        String inputFilePath = "resources/ticket-party.pdf";
-        String outputFilePath = "resources/ticket-party"+ ticket.getTicketID()+ ".pdf";
+        String inputFilePath = "resources/ticketType1-input.pdf";
+        String outputFilePath = "resources/ticketType1-edit.pdf";
 
         File inputFile = new File(inputFilePath);
         PDDocument doc = Loader.loadPDF(inputFile);
         PDPage page = doc.getPage(0);
 
-        // Create a new content stream to write on the page
-        PDPageContentStream contentStream = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, true, true);
+        //Create a new content stream to write on the ticket
         //5th parameters needs to be true to have right coordinate system and text not upside down
+        PDPageContentStream contentStream = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, true, true);
 
 
         PDFont pdfFont = new PDType1Font(Standard14Fonts.FontName.HELVETICA_BOLD);
         contentStream.setFont(pdfFont, 25);
         contentStream.setNonStrokingColor(Color.white);
 
-        // Get the dimensions of the page
         PDRectangle pageSize = page.getMediaBox();
         float pageWidth = pageSize.getWidth();
         float pageHeight = pageSize.getHeight();
 
-        //Set the margin value
-        float marginTop = 20;
+        float margin = 20;
         float x = pageWidth / 4;
         System.out.println(x);
-        float y = pageHeight - marginTop;
+        float y = pageHeight - margin;
 
-        // Write the event information on the page
+        //Write Event Info
         contentStream.beginText();
 
         contentStream.newLineAtOffset(x, y);
@@ -80,14 +88,37 @@ public class TicketLogicManager {
         contentStream.newLine();
         contentStream.showText(event.getLocation());
 
-        contentStream.setTextMatrix(Matrix.getTranslateInstance(x, y - 60));
+        contentStream.setTextMatrix(Matrix.getTranslateInstance(x, y - 50));
         contentStream.newLine();
         contentStream.showText(event.getDate().toString());
 
+        contentStream.setTextMatrix(Matrix.getTranslateInstance(x, y - 70));
+        contentStream.newLine();
+        contentStream.showText("Start time: " + event.getTime().toString());
+
         contentStream.setTextMatrix(Matrix.getTranslateInstance(x, y - 90));
         contentStream.newLine();
-        contentStream.showText(event.getTime().toString());
+        contentStream.showText(event.getNotes());
+
+        contentStream.setTextMatrix(Matrix.getTranslateInstance(x, y - 110));
+        contentStream.newLine();
+        contentStream.showText("End Time: " + event.getEndTime().toString());
+
+        contentStream.setTextMatrix(Matrix.getTranslateInstance(x, y - 130));
+        contentStream.newLine();
+        contentStream.showText("Location guidance: " + event.getLocationGuidance());
+
         contentStream.endText();
+
+
+        //Write QR CODE
+
+        int qrCodeSize = 80;
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(ticket.getTicketID(), BarcodeFormat.QR_CODE, qrCodeSize, qrCodeSize);
+        BufferedImage qrImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+
+        PDImageXObject qrImageXObject = LosslessFactory.createFromImage(doc, qrImage);
+        contentStream.drawImage(qrImageXObject, margin, (pageHeight - qrCodeSize) / 2 + 5, qrCodeSize, qrCodeSize);
 
         contentStream.close();
         File outputFile = new File(outputFilePath);
@@ -95,13 +126,6 @@ public class TicketLogicManager {
         doc.close();
 
         PDDocument editedDoc = Loader.loadPDF(new File(outputFilePath));
-
-        //Read the contents of the page
-        PDFTextStripper stripper = new PDFTextStripper();
-        String pageText = stripper.getText(editedDoc);
-
-        System.out.println(pageText);
-
         editedDoc.close();
     }
 
