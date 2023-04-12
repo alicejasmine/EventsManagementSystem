950da5d8cf87183cebc2cfecc854df72af219de0
AlexKurdelska <alex.kurdelska@gmail.com>
2023-04-12 18:57:59 +0200

    Validate ticket fields

--- src/gui/controller/NewTicketViewController.java
+++ src/gui/controller/NewTicketViewController.java
@@ -6,6 +6,11 @@ import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.stage.*;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NewTicketViewController {
     @FXML
     private TextField customerNameTF;
@@ -18,7 +23,13 @@ public class NewTicketViewController {
     @FXML
     private Button cancelButton;
 
+    private int maxName = 50;
+
+    private int maxEmail = 25;
+
     private Model model = Model.getModel();
+
+
 
     private void createNewTicket(){
         if(!customerNameTF.getText().isEmpty()&&!customerEmailTF.getText().isEmpty()){
@@ -26,12 +37,61 @@ public class NewTicketViewController {
             String customerEmail= customerEmailTF.getText();
             int eventID = model.getSelectedEvent().getId();
             //System.out.println(customerName + customerEmail + eventID);
-            model.addTicket(customerName, customerEmail, eventID);
+            if(!validateNameTextFieldLength() && !validateEmail()) {
+                alertEmailAndNameTextField();
+            } else if (!validateEmail()) {
+                alertEmailValidationTextField();
+            } else if (!validateNameTextFieldLength()) {
+                alertNameTextField();
+            }
+            else {
+                model.addTicket(customerName, customerEmail, eventID);
+            }
             model.loadEventTicketList();
         }
         else{creationErrorLabel.setText("Insert name and email");}
+    }
 
+    private void alertNameTextField() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Validate Name");
+        alert.setContentText("Name is too long, max is 50 characters.");
+        alert.showAndWait();
+    }
 
+    private void alertEmailAndNameTextField(){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Validate Name and Email");
+        alert.setContentText("Name is too long, max is 50 characters. Please enter valid email.");
+        alert.showAndWait();
+    }
+
+    private void alertEmailValidationTextField() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Validate Email");
+        alert.setContentText("Please enter valid email");
+        alert.showAndWait();
+    }
+    private boolean validateNameTextFieldLength() {
+        if(customerNameTF.getText().length() > maxName) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validateEmail() {
+        Pattern p = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$");
+        Matcher m = p.matcher(customerEmailTF.getText());
+
+        if(m.find() && m.group().equals(customerEmailTF.getText()) && !(customerEmailTF.getText().length() > maxEmail)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     private void closeWindow(){
